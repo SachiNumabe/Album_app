7b6d8f31e6e6db09b951a8436218df49c417bde5
numabesachi <numabesachi@gmail.com>
2020-05-18 14:06:55 +0900

    アルバムアプリ完成

---
+++ app/src/main/java/com/example/album_app/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.album_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val preview = Intent(this,PreviewActivity::class.java)
+
+        albumImage1.setOnClickListener {
+            preview.putExtra("image",R.drawable.sea_image)
+            preview.putExtra("title","海")
+            startActivity(preview)
+        }
+
+        albumImage2.setOnClickListener {
+            preview.putExtra("image",R.drawable.cat_image)
+            preview.putExtra("title","猫")
+            startActivity(preview)
+        }
+
+        albumImage3.setOnClickListener {
+            preview.putExtra("image",R.drawable.dog_image)
+            preview.putExtra("title","犬")
+            startActivity(preview)
+        }
+
+        albumImage4.setOnClickListener {
+            preview.putExtra("image",R.drawable.cake_image)
+            preview.putExtra("title","ケーキ")
+            startActivity(preview)
+        }
+
+        albumImage5.setOnClickListener {
+            preview.putExtra("image",R.drawable.night_image)
+            preview.putExtra("title","夜景")
+            startActivity(preview)
+        }
+
+        albumImage6.setOnClickListener {
+            preview.putExtra("image",R.drawable.flower_image)
+            preview.putExtra("title","花")
+            startActivity(preview)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/album_app/PreviewActivity.kt
@@ -0,0 +1,25 @@
+package com.example.album_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_preview.*
+
+class PreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+
+        val imageID = intent.getIntExtra("image",0)
+
+        val displayTitle = intent.getStringExtra("title")
+
+        previewImage.setImageResource(imageID)
+
+        titleText.text = displayTitle
+
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
